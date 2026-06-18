10e64cd8fa6e3314cd5e971b0bf1aae622cc8be3
lucas-rabello-dev <lucasrabello2020@gmail.com>
2026-06-18 17:27:07 -0300

    add ex7

--- exercises/course/ex7.c
+++ exercises/course/ex7.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
 
 // digite um char e retorne dizendo se eh vogal ou consoante
-
-
 int main(void)
 {
 	char c; scanf("%c", &c);
 
-	switch(c)
-	{
-		case 'a':
-			printf("Eh uma vogal\n");
-			break;
-		case 'e':
-			printf("Eh uma vogal\n");
-			break;
-		case 'i':
-			printf("Eh uma vogal\n");
-			break;
-		case 'o':
-			printf("Eh uma vogal\n");
-			break;
-		case 'u':
-			printf("Eh uma vogal\n");
-			break;
-		default:
-			printf("Eh uma consoante\n");
-	}
+	char vogais[] = {'a', 'e', 'i', 'o', 'u'};
 
-	
+	for (int i = 0; i < sizeof(vogais); i++)
+	{		
+		if (c == vogais[i])
+		{
+			printf("Eh uma vogal \n");
+			return 0;
+		}
+	}
+	printf("nao eh uma vogal \n");
 	return 0;
 }
